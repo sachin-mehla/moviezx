90aa28c83307f9b036d437102034c8916a26f474
sachin-mehla <schnmehla@gmail.com>
2022-10-01 11:38:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/sachin-mehla/moviezx /moviezx
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /moviezx
-fi
-cd /moviezx
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/JOSProjects/IMDb-Movie-Bot.git /IMDb-Movie-Bot
+cd /IMDb-Movie-Bot
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot, Please Wait..."
 python3 bot.py
