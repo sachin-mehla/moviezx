9691c6723c6b198f89cb803ba11c88d9b5ccad6e
sachin-mehla <schnmehla@gmail.com>
2022-11-01 18:28:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rorboy:rorboy@cluster0.2erhwjr.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ror")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Hashtag')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'hashtag')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001526478235'))
